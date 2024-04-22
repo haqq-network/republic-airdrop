a04bc2d6e505eee30b15f3ae90ddb81ba1859405
Rinat <rinatficht@gmail.com>
2024-04-22 15:58:06 +0300

    fix: removed email

--- libs/main-page/src/lib/client/top-block/purchase-failure-block/purchase-failure-block.tsx
+++ libs/main-page/src/lib/client/top-block/purchase-failure-block/purchase-failure-block.tsx
@@ -4,10 +4,7 @@ export function PurchaseFailureBlock({ className }: { className?: string }) {
   return (
     <div className={clsx('max-w-[600px] text-[18px]', className)}>
       Unfortunately, the address of the connected wallet is not on the list of
-      ISLM purchasers on Republic. If that's not the case email us at
-      <a className="text-main-red ml-[4px]" href="mailto:problem@haqq.network">
-        problem@haqq.network
-      </a>
+      ISLM purchasers on Republic.
     </div>
   );
 }
